e53ba6f385be0b83b0738710f77a76a278f1f87b
Ometere-Godwin <ookuoluse@yahoo.com>
2024-08-02 16:51:13 +0100

    some files editted

--- components/CryptoDataDispay.tsx
+++ components/CryptoDataDispay.tsx
@@ -37,7 +37,11 @@ export default function CryptoDataDispay() {
         const data = await response.json();
         setCryptoData(data);
       } catch (err) {
-        setError(err.message);
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occured");
+        }
       } finally {
         setLoading(false);
       }
